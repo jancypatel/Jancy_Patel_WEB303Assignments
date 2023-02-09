e44ca021b84ac72ce241632931d1c88e8fa05935
jancypatel <113460020+jancypatel@users.noreply.github.com>
2023-02-08 19:57:26 -0500

    WEB303 Assignment 2

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// WEB303 Assignment 2
+
+// Name: Jancy Patel
+
+$(function(){
+      const prospect = $('#prospect');
+      const convert = $('#convert');
+      const retain = $('#retain');
+  
+      const content = $('#content');
+  
+      function getContent(e) {
+          content.slideUp(500, ()=>{
+  
+              const fileName = `${e.target.id}.html`;
+      
+              const xhr = new XMLHttpRequest();
+              xhr.open('GET', fileName, true);
+      
+              xhr.onload = function(){
+                  content.html(this.responseText);
+                  content.slideDown();
+              }
+      
+              xhr.send();
+          });
+  
+      }
+      prospect.click(getContent);
+      convert.click(getContent);
+      retain.click(getContent);
+  });
+
+
